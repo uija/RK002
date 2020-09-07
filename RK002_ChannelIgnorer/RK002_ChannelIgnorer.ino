d61ab2736839d85caddc27223060b6c511d0cbc5
Jens Kirchheimer <jens.kirchheimer@calumetphoto.de>
2020-09-07 05:37:32 +0200

    Added Channel Ignorer

---
+++ RK002_ChannelIgnorer/RK002_ChannelIgnorer.ino
@@ -0,0 +1,40 @@
+#include <RK002.h>
+
+#define MAXCHANNEL 9
+ 
+RK002_DECLARE_INFO("Channel Ignorer","jkirchheimer@gmail.com ","1.0","ccd6aa34-84a8-484b-bfec-5987c3cf36dc")
+
+boolean checkChannel( byte channel) {
+  return channel <= MAXCHANNEL;
+}
+
+boolean RK002_onNoteOff(byte channel, byte key, byte velocity) {
+  return checkChannel( channel);
+}
+boolean RK002_onNoteOn(byte channel, byte key, byte velocity) {
+  return checkChannel( channel);
+}
+boolean RK002_onPolyPressure(byte channel, byte key, byte value) {
+  return checkChannel( channel);
+}
+boolean RK002_onControlChange(byte channel, byte nr, byte value) {
+  return checkChannel( channel);
+}
+boolean RK002_onProgramChange(byte channel, byte nr) {
+  return checkChannel( channel);
+}
+boolean RK002_onChannelPressure(byte channel, byte val) {
+  return checkChannel( channel);
+}
+boolean RK002_onPitchBend(byte channel, int val) {
+  return checkChannel( channel);
+}
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
